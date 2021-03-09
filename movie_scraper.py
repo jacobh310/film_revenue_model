1535b746c441313940e560637a85da6e040039e1
Jacob Hernandez <jacob.hernandez0830@gmail.com>
2021-03-09 09:14:00 -0800

    made changes to the scraper and jupyer notebook.

--- movie_scraper.py
+++ movie_scraper.py
@@ -4,20 +4,23 @@ import pandas as pd
 
 movie_list = pd.read_csv('movie_links.csv', names=['title','link'])
 
-urls = movie_list['link']
-# urls = ['https://en.wikipedia.org/wiki/127_Hours', 'https://en.wikipedia.org/wiki/25th_Hour']
+urls = movie_list['link'][:15]
+# urls = ['https://en.wikipedia.org/wiki/127_Hours', 'https://en.wikipedia.org/wiki/25th_Hour','https://en.wikipedia.org//wiki/Zero_Dark_Thirty']
 movies_df = pd.DataFrame()
 count = 1
 for url in urls:
     page = requests.get(url)
     soup = bs(page.text, 'lxml')
+    title = soup.find_all(id='firstHeading')[0].i.text
+
     try:
         summary = soup.find_all(name='table',class_='infobox')[0]
     except:
         continue
 
     infobox = {}
-    print(count, movie_list['title'][count-1])
+    infobox['Title'] = title
+    print(count, title)
 
     for tr in summary.find_all('tr')[2:]:
         try:
@@ -37,9 +40,8 @@ for url in urls:
                 for li in ul.find_all('li'):
                     items.append(li.text)
                 infobox[col].append(items)
-    infobox['title'] = movie_list['title'][count-1]
-    count += 1
 
+    count += 1
     movies_df = movies_df.append(infobox, ignore_index=True)
 
-movies_df.to_csv('wiki_data.csv')
+# movies_df.to_csv('wiki_data.csv')
